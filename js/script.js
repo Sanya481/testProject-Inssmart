c703e93a366f1ba3f8d0410b18853c0a8d17110d
Eremeev_Alexander <sanya.eremey@mail.ru>
2022-12-18 17:26:47 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,70 +1,3 @@
-/*  P.S. Не очень универсальный способ переключения табов. Через значение data атрибута */
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const body = document.body;
-//     const tabBtns = document.querySelectorAll('[data-tab-btn]');
-//     const tabContents = document.querySelectorAll('[data-tab-content]');
-
-//     body.addEventListener('click', (evt) => {
-//         if (evt.target.matches('[data-tab-btn]')) {
-//             const tabBtnData = evt.target.dataset.tabBtn;
-//             switchTabContent(tabBtnData);
-//         }
-//     })
-
-//     /**
-//      * @description Изменение контента в зависимости от нажатой кнопки
-//      * @param {string} tabBtnData - значение дата атрибута у кнопки по которой кликнул пользователь
-//      */
-//     const switchTabContent = (tabBtnData) => {
-//         tabBtns.forEach(btn => {btn.classList.remove('active')});
-//         document.querySelector(`[data-tab-btn="${tabBtnData}"]`).classList.add('active');
-
-//         tabContents.forEach(content => {content.classList.remove('active')});
-//         document.querySelector(`[data-tab-content="${tabBtnData}"]`).classList.add('active');
-//     }
-
-// })
-
-// const body = document.body;
-// const tabBtns = Array.from(document.querySelectorAll('[data-tab-btn]'));
-// const tabContents = Array.from(document.querySelectorAll('[data-tab-content]'));
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   // Инициализировали активные табы
-//   tabBtns[0].classList.add('active');
-//   tabContents[0].classList.add('active');
-
-//   // Повесили обработчик клика на тег body
-//   body.addEventListener('click', (evt) => {
-//     // и если клик был по кнопке с нужным атрибутом
-//     if (evt.target.matches('[data-tab-btn]')) {
-//       // берем эту кнопку
-//       const currentTab = evt.target;
-//       // индекс кнопки (индекс кнопки, совпадает с индексом контента - на индексах эл-тов построено переключение между табами)
-//       const indexTab = tabBtns.indexOf(currentTab);
-//       // если у кнопки по ко-ой кликнули не имеет класс active
-//       if (!currentTab.classList.contains('active')) {
-//         // проходимся по всем кнопкам и убираем класс active
-//         tabBtns.forEach((tab) => {
-//           tab.classList.remove('active');
-//         })
-//         // а нужной кнопке вешаем класс active
-//         currentTab.classList.add('active');
-//         // то же самое с контентом - убираем класс active
-//         tabContents.forEach((tab) => {
-//           tab.classList.remove('active');
-//         })
-//         // и контенту, индекс которого совпадает с индексом кнопки - вешаем класс active
-//         tabContents[indexTab].classList.add('active');
-//         tabContents[indexTab].style.maxHeight = tabContents[indexTab].offsetHeight + 'px';
-//         // .style.maxHeight = offsetHeight + 'px';
-//       }
-
-//     }
-//   })
-// })
-
 // Добавление характеристик
 
 /**
@@ -80,57 +13,91 @@ const formInputFields = document.querySelectorAll('[data-good-characteristic]');
 // Получение шаблона для заполнения
 const templateFragment = document.querySelector('#error-message').content;
 const template = templateFragment.querySelector('[data-error-message]');
-const elements = template.cloneNode(true);
+
 
 /**
  * @description Создание характеристики введенное пользователем и добавление в список характеристик
  * @param {HTMLElement} optionList - место для вставки сообщения
  * @param {HTMLElement} option - поле ввода характеристики
+ * @param {HTMLElement} form - форма ввода хар-ки
  */
-const createElement = (optionList, option) => {
+const createElement = (optionList, option, form) => {
   const characteristic = document.createElement('li');
   characteristic.dataset.option = '';
   characteristic.textContent = option.value;
 
+  // Кнопка добавления хар-ки
+  const submitBtn = form.querySelector('[data-submit-btn]');
+  // Место куда добавлять сообщение об ошибке
+  const formGroup = form.querySelector('[data-form-group]');
 
-  optionList.append(characteristic);
-  // Очищаем поле ввода после добавления хар-ки
-  option.value = '';
+  // Находим все хар-ки
+  const characteristics = optionList.querySelectorAll('[data-option]');
+  // Создаем пустой массив для значений (характеристик в ввиде строки)
+  const characteristicsString = [];
+  // Извлекаем из списка хар-ик только строки и добавляем в массив
+  characteristics.forEach((item) => {
+    characteristicsString.push(item.textContent);
+  })
 
+  // Проверка на похожие хар-ки
+  if (characteristicsString.includes(option.value)) {
+    // Скопировали из шаблона разметку
+    const elements = template.cloneNode(true);
+    // Поменяли текст
+    elements.querySelector('[data-error-text]')
+      .textContent = 'Похоже эта характеристика уже существует!';
+
+    formGroup.after(elements);
+    submitBtn.disabled = true;
+
+    // Проверка, если вводится хар-ка на английском
+  } else if (/[a-zA-Z]/.test(option.value)) {
+    // Скопировали из шаблона разметку
+    const elements = template.cloneNode(true);
+    // Поменяли текст
+    elements.querySelector('[data-error-text]')
+      .textContent = 'Можно по-русски пжлст:) Ай донт спик инглиш!';
+
+    formGroup.after(elements);
+    submitBtn.disabled = true;
+  } else {
+    optionList.append(characteristic);
+    // Очищаем поле ввода после добавления хар-ки
+    option.value = '';
+  }
 }
 
-
-
 if (characteristicForms) {
-
   characteristicForms.forEach((form) => {
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
+      // Форма
       const currentForm = evt.target;
+      // Обертка формы
       const formWrapper = currentForm.querySelector('[data-form-wrapper]');
+      // Поле для ввода хар-ки
       const goodCharacteristic = currentForm.querySelector('[data-good-characteristic]');
+      // Блок с полем ввода
       const formGroup = formWrapper.querySelector('[data-form-group]');
-
+      // Общий блок с хар-ми
       const characteristicBlock = currentForm.closest('[data-characteristic-block]');
-
+      // Список с хар-ми
       const characteristicList = characteristicBlock.querySelector('[data-characteristic-list]');
+      // Кнопка добавления хар-ки
+      const submitBtn = currentForm.querySelector('[data-submit-btn]');
 
+      const elements = template.cloneNode(true);
 
       // Если поле для ввода хар-ки не пустое
       if (goodCharacteristic.value.trim() != "") {
 
-        // checkRepeatOption(goodCharacteristic.value, characteristicBlock);
-
         // Код внизу - подробное описание функции
-        createElement(characteristicList, goodCharacteristic);
+        createElement(characteristicList, goodCharacteristic, currentForm);
         // const characteristic = document.createElement('li');
         // characteristic.dataset.option = '';
         // characteristic.textContent = goodCharacteristic.value;
         // characteristicList.append(characteristic);
-
-
-
-
 
         // Если есть сообщение об ошибке ввода, когда пользователь ввел характеристику - удаляем сообщение
         if (elements) {
@@ -139,6 +106,7 @@ if (characteristicForms) {
         // Если пол-ль пытается отправить пустую строку - выводим сообщение
       } else {
         formGroup.after(elements);
+        submitBtn.disabled = true;
       }
     })
   })
@@ -148,27 +116,36 @@ if (characteristicForms) {
     field.addEventListener('input', (evt) => {
       const currentForm = evt.target.closest('[data-characteristic-form]');
       const errorMessage = currentForm.querySelector('[data-error-message]');
+      // Кнопка добавления хар-ки
+      const submitBtn = currentForm.querySelector('[data-submit-btn]');
 
       // Если поле не пустое и есть сообщение - удалим сообщение
       if (evt.target.value != "" && errorMessage) {
         errorMessage.remove();
+        submitBtn.disabled = false;
+      }
+
+      if (evt.target.value === "" && errorMessage) {
+        errorMessage.remove();
+        submitBtn.disabled = false;
       }
     })
   })
-
 }
 
-
+// Массив кнопок - табов
 const tabBtns = Array.from(document.querySelectorAll('[data-tab-btn]'));
+// Массив контента
 const tabContents = Array.from(document.querySelectorAll('[data-tab-content]'));
+// Обертка табов
 const tabItems = Array.from(document.querySelectorAll('[data-tab-item]'));
+// Общая обертка сайта
 const bodyWrapper = document.querySelector('[data-body-wrapper]')
-
+// Общий блок с табами
 const tabs = document.querySelector('[data-tabs-section]');
 
 // Инициализация активных эл-ов
 document.addEventListener('DOMContentLoaded', () => {
-
   if (window.matchMedia('(max-width: 968px)').matches) {
     tabItems[0].classList.add('is-open');
     tabContents[0].style.maxHeight = tabContents[0].scrollHeight + 'px';
@@ -178,12 +155,10 @@ document.addEventListener('DOMContentLoaded', () => {
     tabItems[0].classList.add('is-active');
     bodyWrapper.style.paddingBottom = tabContents[0].scrollHeight + 'px';
   }
-
 })
 
 /* Переключение табов происходит при добавлении класса к эл-ту tabs__item и у какого эл-та есть класс тот таб и открываем - такая реализация из-за разметки */
 if (tabs) {
-
   tabs.addEventListener('click', (evt) => {
     if (evt.target.matches('[data-tab-btn]')) {
       const tabItem = evt.target.closest('[data-tab-item]');
@@ -202,7 +177,6 @@ if (tabs) {
 
       // Десктопная версия
       if (window.matchMedia('(min-width: 969px)').matches) {
-
         tabItems.forEach((item) => {
           item.classList.remove('is-active');
           tabItem.classList.add('is-active');
@@ -213,11 +187,3 @@ if (tabs) {
     }
   })
 }
-
-
-
-
-
-
-
-
